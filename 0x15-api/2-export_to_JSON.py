e76c224489ca1b6eeeb106b643aae6d823e6944c
Your Name <you@example.com>
2023-05-22 13:23:42 -0700

    API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
-
+""" a Python script that, using a REST API, for a given employee ID,
+    returns information about his/her TODO list progress."""
 import json
 import requests
 import sys
 
 
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
-
-    response = requests.get(url)
-    username = response.json().get('username')
-
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-
-    dictionary = {employeeId: []}
-    for task in tasks:
-        dictionary[employeeId].append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": username
-        })
-    with open('{}.json'.format(employeeId), 'w') as filename:
-        json.dump(dictionary, filename)
+    url = 'https://jsonplaceholder.typicode.com/users?id=' + sys.argv[1]
+    r = requests.get(url)
+    if r.status_code == 200:
+        data = {sys.argv[1]: []}
+        username = r.json()[0].get("username")
+        url2 = 'https://jsonplaceholder.typicode.com/todos'
+        r2 = requests.get(url2)
+        for item in r2.json():
+            if item.get("userId") == int(sys.argv[1]):
+                d = {'task': item.get('title'),
+                     'completed': item.get('completed'),
+                     'username': username}
+                data[sys.argv[1]].append(d)
+    filename = sys.argv[1] + '.json'
+    with open(filename, 'w') as f:
+        json.dump(data, f)
