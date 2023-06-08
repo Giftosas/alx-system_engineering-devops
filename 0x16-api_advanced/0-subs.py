e7ee0dcea4bbcfaee91a1e88e30b9e169e2ae029
Your Name <you@example.com>
2023-06-08 14:43:04 -0700

    completed task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-""" gets the number of subs for a subreddit """
-import json
-import requests
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 11:47:53 2020
+
+@author: Robinson Montes
+"""
+from json import loads
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """ a function that takes a subbreddit and returns the number of subs"""
-    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
-    data = requests.get("https://www.reddit.com/r/{}/about.json"
-                        .format(subreddit),
-                        headers=user_agent,
-                        allow_redirects=False)
-    if data.status_code != 200:
+    """ecursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no
+    results are found for the given subreddit, the function should return None
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = get(url, headers=headers)
+    reddits = response.json()
+
+    try:
+        subscribers = reddits.get('data').get('subscribers')
+        return int(subscribers)
+    except:
         return 0
-    json_data = data.json()
-    results = json_data.get("data").get("subscribers")
-    return results
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-""" gets top 10 post on subreddit """
-import json
-import requests
+"""
+Created on Thu Sep 17 11:47:53 2020
+
+@author: Robinson Montes
+"""
+from json import loads
+from requests import get
 
 
 def top_ten(subreddit):
-    """ gets the top 10 post of a subreddit"""
-    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
-    data = requests.get("https://www.reddit.com/r/{}/hot/.json"
-                        .format(subreddit),
-                        headers=user_agent,
-                        allow_redirects=False)
-    if data.status_code != 200:
-        print("None")
-        return
-    json_data = data.json().get("data").get("children", [])
-    if len(json_data) == 0:
-        print("None")
-        return
-    for post in json_data[0:10]:
-        print(post.get("data").get("title"))
+    """queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = get(url, headers=headers, allow_redirects=False)
+    reddits = response.json()
+
+    try:
+        children = reddits.get('data').get('children')
+        for i in range(10):
+            print(children[i].get('data').get('title'))
+    except:
+        print('None')
